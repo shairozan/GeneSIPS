f0138d694036d19636448224d2b989c4ccec9854
Darrell Breeden <darrell.breeden@cumulose.com>
2019-01-17 21:13:41 -0500

    Tried regex test, but having trouble getting it to pass in c#. Passes in every other language / environment O.o

---
+++ GeneTests/HeaderTests.cs
@@ -0,0 +1,20 @@
+﻿using GeneSIPs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using System.Linq;
+
+namespace GeneTests
+{
+    public class HeaderTests
+    {
+        [Fact]
+        public void VerifyFromIsValid()
+        {
+            SIPMessage message = SIPMessage.Faker.Generate(1).First();
+            Assert.True(message.Header.From.IsValid());
+        }
+        
+    }
+}
