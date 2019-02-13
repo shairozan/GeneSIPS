840d0fac7f55eefa888342f3b5c7209a1697d0e5
Darrell Breeden <dukeofubuntu@gmail.com>
2019-02-12 21:57:13 -0500

    Dealing with issue #7. Making sure when using Faker a GUID is generated for the tag field to be RFC compliant

--- GeneSIPs/Common/SipAddress.cs
+++ GeneSIPs/Common/SipAddress.cs
@@ -9,6 +9,8 @@ namespace GeneSIPs.Common
     {
         public string User { get; set; }
         public string Domain { get; set; }
+        //TODO: RFC 3261 Section 19.3 Indicates that both the To and From header fields should have a tag value
+        //But no samples I've seen actually have them. Even in the RFC. Only From Tags have them in their value.
         public static Faker<SipAddress> Faker { get; set; } = new Faker<SipAddress>()
             .StrictMode(true)
             .RuleFor(o => o.User, f => f.Internet.Email().Split('@')[0])
--- GeneSIPs/Header/From.cs
+++ GeneSIPs/Header/From.cs
@@ -15,7 +15,7 @@ namespace GeneSIPs.Header
         /// </summary>
         public string DisplayName { get; set; }
         public SipAddress User { get; set; }
-        public string Tag { get; set; }
+        public string Tag { get; set; } //Required per RFC 3261 Section 19.3
         // ^From: \"[a-zA-Z -_]{1,}\" <[a-zA-z0-9._]{1,}@[a-zA-Z0-9._]{1,}>(?:tag=[a-z0-9]{1,8})?$ is raw regex
         private Regex Validity = new Regex(@"^From: .[a-zA-Z-_]{1,}. <[a-zA-z0-9._]{1,}@[a-zA-Z0-9._]{1,}>(?:tag=[a-z0-9]{1,8})?$\r\n");
         
@@ -30,7 +30,7 @@ namespace GeneSIPs.Header
             .StrictMode(false)
             .RuleFor(o => o.DisplayName, f => f.Name.FullName())
             .RuleFor(o => o.User, f => SipAddress.Faker.Generate(1).First())
-            .RuleFor(o => o.Tag, f => f.Random.String2(5, "abcdefghijklmopqrstuvwxyz"));
+            .RuleFor(o => o.Tag, f => Guid.NewGuid().ToString());
 
         public override string ToString()
         {
